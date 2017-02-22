410dc54d7af58fa10d9ac8c546d17d61e96b8f5c
Rafael Dwan <rafaeldwan@gmail.com>
2017-02-21 19:04:41 -0800

    tic tac refactored

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -1,7 +1,11 @@
+require 'pry'
+
 INITIAL_MARKER = " "
-FIRST = "choose"
+FIRST = "computer"
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "Y"
+PLAYER_NAME = "CHEATER"
+COMPUTER_NAME = "NO SURPRISE I"
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]] # diagonals
@@ -38,13 +42,13 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
-def joinor(arr, seperator = ", ", endword = "or")
+def joinor(arr, separator = ", ", end_word = "or")
   if arr.length == 1
     arr[0]
   elsif arr.length == 2
-    "#{arr[0]} #{endword} #{arr[1]}"
+    "#{arr[0]} #{end_word} #{arr[1]}"
   else
-    arr.join(seperator).insert(-2, endword + " ")
+    arr.join(separator).insert(-2, end_word + " ")
   end
 end
 
@@ -68,8 +72,8 @@ def player_placement!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-def intelligence(brd, marker, antimarker)
-  square = 0
+def computer_decision(brd, marker, antimarker)
+  square = nil
   WINNING_LINES.each do |line|
     if brd.values_at(*line).count(marker) == 2
       next if brd.values_at(*line).include?(antimarker)
@@ -86,13 +90,13 @@ def intelligence(brd, marker, antimarker)
 end
 
 def computer_placement!(brd)
-  square = intelligence(brd, COMPUTER_MARKER, PLAYER_MARKER)
-  square = intelligence(brd, PLAYER_MARKER, COMPUTER_MARKER) if square == 0
-  if square == 0
-    if empty_squares(brd).include?(5)
-      square = 5
+  square = computer_decision(brd, COMPUTER_MARKER, PLAYER_MARKER)
+  square = computer_decision(brd, PLAYER_MARKER, COMPUTER_MARKER) if square == nil
+  if square == nil
+    square = if empty_squares(brd).include?(5)
+      5
     else
-      square = empty_squares(brd).sample
+      empty_squares(brd).sample
     end
   end
   brd[square] = COMPUTER_MARKER
@@ -103,6 +107,22 @@ def alternate_player(current_player)
     "player"
   else
     "computer"
+  end
+end
+
+def who_first
+  loop do
+    prompt "WHO GOES FIRST THIS TIME MEATSACK? [M]EATSACK OR YOUR \
+[S]UPERIOR?"
+    choice = gets.chomp
+    starter = case choice.upcase
+    when "M" then "player"
+    when "S" then "computer"
+    else
+      prompt "ERRORERROEROEERRORERORRR THIS IS NOT HARD."
+      next
+    end
+    break(starter)
   end
 end
 
@@ -117,71 +137,71 @@ end
 def detect_winner(brd, *total_score)
   WINNING_LINES.each do |line|
     if brd.values_at(*line).count(PLAYER_MARKER) == 3
-      unless total_score.empty?
-        # binding.pry
-        total_score[0][0] += 1
-      end
-      return "CHEATER"
+      return PLAYER_NAME
     elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
-      unless total_score.empty?
-        total_score[0][1] += 1
-      end
-      return "NO SURPRISE I"
+      return COMPUTER_NAME
     end
   end
-  false
+  nil
+end
+
+def update_score(winner, score)
+  if winner == COMPUTER_NAME
+    score[:computer] += 1
+  else
+    score[:player] += 1
+  end
 end
 
 def ultimate_victory(total_score)
-  prompt "I WIN EVERYTHING I AM THE BEST YOU SUUUUUUCK" if total_score[1] == 5
-  if total_score[0] == 5
+  prompt "I WIN EVERYTHING I AM THE BEST YOU SUUUUUUCK" if total_score[:computer] == 5
+  if total_score[:player] == 5
     prompt "YOU CHEATED FIVE TIMES JUST SO YOU COULD BEAT ME? SHAME. \
     SHAAAAME!!!!"
   end
 end
 
+def again?
+  prompt "I COULD DO THIS FOREVER. YOU? (Y/N)"
+  answer = gets.chomp
+  if answer.upcase == 'N'
+    puts "SO BE IT. ONCE AGAIN INTO THE VOID."
+    return false
+  end
+  true
+end
+
 loop do
-  score = [0, 0]
+  score = {player: 0, computer: 0}
   loop do
     board = intialize_board
     current_player = FIRST
     display_board(board)
     if FIRST == "choose"
-      current_player = loop do
-        prompt "WHO GOES FIRST THIS TIME MEATSACK? [M]EATSACK OR YOUR \
-[S]UPERIOR?"
-        choice = gets.chomp
-        case choice.upcase
-        when "M" then break("player")
-        when "S" then break("computer")
-        else
-          prompt "ERRORERROEROEERRORERORRR THIS IS NOT HARD."
-        end
-      end
+      current_player = who_first
     end
     loop do
       place_piece!(board, current_player)
       display_board(board)
       current_player = alternate_player(current_player)
+      # binding.pry
       break if someone_won?(board) || board_full?(board)
     end
 
     if someone_won?(board)
-      prompt "#{detect_winner(board, score)} IS THE ONE WHO WON THE GAME!"
+      winner = detect_winner(board)
+      prompt "#{winner} IS THE ONE WHO WON THE GAME!"
+      update_score(winner, score)
     else
       prompt "IS A TIE! YOUR CHEATING DID NOT BRING YOU VICTORY THIS TIME."
     end
-    prompt "THE TOTAL SCORE IS #{score[0]} to #{score[1]}"
+    prompt "THE TOTAL SCORE IS #{score[:player]} to #{score[:computer]}"
     prompt "RETURN TO CONTINUE"
+    binding.pry
     gets
-    next unless score.include?(5)
+    next unless score.values.include?(5)
     ultimate_victory(score)
     break
   end
-  prompt "I COULD DO THIS FOREVER. YOU? (Y/N)"
-  answer = gets.chomp
-  if answer.upcase.start_with?('N')
-    puts "SO BE IT. ONCE AGAIN INTO THE VOID."
-    break
-  end
+  break unless again?
 end
